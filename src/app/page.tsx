7ea00d608f7f62b530a19b8cc6339617711792b0
Maria Namata <marianamata086@gmail.com>
2024-04-13 00:57:55 +0300

    Add more configuration files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
+  let food;
   return (
-    <main className="items-center justify-center text-xl text-teal-500">
-      <Link href={"/about"}>About us</Link>
-      <p className="h-[500px]"></p>
+    <main className='items-center justify-center text-xl text-teal-500'>
+      <Link href={'/about'}>About us</Link>
+      <p className='h-[500px]'></p>
       Hello Welcome to Lwevuze&#39;s website
     </main>
   );
