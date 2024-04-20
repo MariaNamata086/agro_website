3d3bc76c7e284cb7c38179db20ce7bfe997db8f1
Maria Namata <namatamaria086@gmail.com>
2024-04-20 22:53:45 +0300

    More authentication tests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className='items-center justify-center text-xl text-teal-500'>
       <Link href={'/about'}>About us</Link>
       <p className='h-[500px]'></p>
-      Welcome to Lwevuze&#39;s website
+      Welcome to Lwevuze&#39;s farm website
     </main>
   );
 }
